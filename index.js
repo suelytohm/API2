9d0a40e223de944a5fc429517e6c9b17ce2c26c4
suelytohm <suelytohm.02@gmail.com>
2022-07-10 03:01:22 -0300

    port

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD
 mongoose.connect('mongodb+srv://' + DB_USER + ':' + DB_PASSWORD + '@cluster0.ft2n9.mongodb.net/?retryWrites=true&w=majority')
 .then(() => {
     console.log('Conectamos ao mongo!')
-    app.listen(3000)
+    app.listen(process.env.PORT || 3000)
 })
 .catch((err) => console.log(err))
 
